4954e9a99cd683b394b5e37f93db670478c1c8df
Michal Kuspit <michal.kuspit@gmail.com>
2021-07-09 14:18:18 +0400

    Add lambdas.py, map.py

---
+++ lambdas.py
@@ -0,0 +1,9 @@
+def square(num):
+	return num ** 2
+square(2)
+
+def square_2(num): return num ** 2
+square_2(3)
+
+square_3 = lambda num: num * num
+square_3(4)
---
+++ map.py
@@ -0,0 +1,6 @@
+nums = [2, 4, 6, 8, 10]
+
+doubles = map(lambda x: 2*x, nums)
+
+print(type(doubles))
+print(list(doubles))
