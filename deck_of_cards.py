589b663df7ffcfc6f74badddbdf4d13362c6a8e1
Michal Kuspit <michal.kuspit@gmail.com>
2021-08-21 10:23:15 +0400

    Update  deck_of_cards.py

--- deck_of_cards.py
+++ deck_of_cards.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 class Card:
 	def __init__(self, value, suit):
 		self.value = value
@@ -14,23 +16,35 @@ class Deck:
 		values = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
 		self.cards = [Card(v, s) for s in suits for v in values]
 
+
 	def __repr__(self):
 		return f"Deck of {self.count()} cards"
 
+
 	def _deal(self, num):
 		count = self.count()
-		act_num = min([count, num])
-		
+		c = min([count, num])
+		if count == 0:
+			raise ValueError("All cards have been dealt")
+		cards = self.cards[-actual:]
+		self.cards = self.cards[:-actual]
+		return cards
+
 
 	def shuffle(self):
+		if self.count() < 52:
+			raise ValueError("Only full decks can be shuffled")
+		shuffle(self.cards)
+		return self
 
 
 	def deal_card(self):
+		return self._deal(1)[0]
 
 
-	def deal_hand(self, num_cards):
+	def deal_hand(self, hand_size):
+		return self._deal(hand_size)
 
 
 	def count(self):
 		return len(self.cards)
-
